6c213fb1b6449b39b9c9bb134e257c6262e258d7
Lion786420 <thapaaniket12345@gmail.com>
2024-01-19 11:51:47 +0545

    Added year filter functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,30 +9,21 @@ const Expenses = (props) => {
   const yearIndicator = (yearSelected) => {
     setFilteredYear(yearSelected);
   };
+  const filteredExpense = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className="expenses">
       <ExpenseFilter yearSender={yearIndicator} selected={filteredYear} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {filteredExpense.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
